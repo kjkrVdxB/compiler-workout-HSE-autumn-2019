51a28070448383ee662dfab34a6e826c2836a172
Your Name <you@example.com>
2019-10-15 03:06:23 +0300

    Finish hw7

--- src/Language.ml
+++ src/Language.ml
@@ -35,6 +35,10 @@ module State =
     (* Drops a scope *)
     let leave st st' = {st' with g = st.g}
 
+    let rec update_many xs vs s = match xs with
+      | [] -> s
+      | x::xs -> update_many xs (List.tl vs) (update x (List.hd vs) s)
+
   end
     
 (* Simple expressions: syntax and semantics *)
@@ -74,7 +78,48 @@ module Expr =
        which takes an environment (of the same type), a name of the function, a list of actual parameters and a configuration, 
        an returns resulting configuration
     *)                                                       
-    let rec eval env ((st, i, o, r) as conf) expr = failwith "Not implemented"
+    let to_func op =
+      let bti   = function true -> 1 | _ -> 0 in
+      let itb b = b <> 0 in
+      let (|>) f g   = fun x y -> f (g x y) in
+      match op with
+      | "+"  -> (+)
+      | "-"  -> (-)
+      | "*"  -> ( * )
+      | "/"  -> (/)
+      | "%"  -> (mod)
+      | "<"  -> bti |> (< )
+      | "<=" -> bti |> (<=)
+      | ">"  -> bti |> (> )
+      | ">=" -> bti |> (>=)
+      | "==" -> bti |> (= )
+      | "!=" -> bti |> (<>)
+      | "&&" -> fun x y -> bti (itb x && itb y)
+      | "!!" -> fun x y -> bti (itb x || itb y)
+      | _    -> failwith (Printf.sprintf "Unknown binary operator %s" op)
+
+
+    let rec eval_list env config es =
+      List.fold_left
+        (fun (vs, config) e ->
+          let config = eval env config e in
+          let s, i, o, Some r = config in
+          vs @ [r], (s, i, o, None)) ([], config) es
+    and eval env ((s, i, o, _) as config) expr =
+      match expr with
+      | Const n ->
+         (s, i, o, Some n)
+      | Var   x ->
+         (s, i, o, Some State.eval s x)
+      | Binop (op, x, y) ->
+         let config = eval env config x in
+         let _, _, _, Some r1 = config in
+         let config = eval env config y in
+         let s, i, o, Some r2 = config in
+         (s, i, o, Some to_func op r1 r2)
+      | Call (f, args) ->
+         let argvals, config = eval_list env config args in
+         env#definition env f argvals config
          
     (* Expression parser. You can use the following terminals:
 
@@ -82,7 +127,27 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string                                                                                                                  
     *)
     ostap (                                      
-      parse: empty {failwith "Not implemented"}
+      parse:
+	  !(Ostap.Util.expr
+              (fun x -> x)
+	     (Array.map (fun (a, s) -> a,
+                           List.map  (fun s -> ostap(- $(s)), (fun x y -> Binop (s, x, y))) s
+                        )
+              [|
+		`Lefta, ["!!"];
+		`Lefta, ["&&"];
+		`Nona , ["=="; "!="; "<="; "<"; ">="; ">"];
+		`Lefta, ["+" ; "-"];
+		`Lefta, ["*" ; "/"; "%"];
+              |]
+	     )
+	     primary);
+
+      primary:
+        n:DECIMAL {Const n}
+      | fun_name:IDENT "(" args:!(Util.list0By)[ostap (",")][parse] ")" { Call (fun_name, args) }
+      | x:IDENT   {Var x}
+      | -"(" parse -")"
     )
     
   end
@@ -111,11 +176,62 @@ module Stmt =
        Takes an environment, a configuration and a statement, and returns another configuration. The 
        environment is the same as for expressions
     *)
-    let rec eval env ((st, i, o, r) as conf) k stmt = failwith "Not implemnted"
+    let rec eval env ((s, i, o, _) as config) k stmt = match stmt with
+        | Read name ->
+           let z :: i = i in
+           eval env (State.update name z s, i, o, None) Skip k
+        | Write ex ->
+           let s, i, o, Some r = Expr.eval env config ex in
+           eval env (s, i, o @ [r], None) Skip k
+        | Assign (name, ex) ->
+           let s, i, o, Some r = Expr.eval env config ex in
+           eval env (State.update name r s, i, o, None) Skip k
+        | Seq (a, b) ->
+           let cont = if k = Skip then b else Seq (b, k) in
+           eval env config cont a
+        | Skip -> (match k with Skip -> config | _ -> eval env config Skip k)
+        | If (c, t, f) ->
+          let config = Expr.eval env config c in
+          let s, i, o, Some r = config in
+          if r <> 0 then eval env (s, i, o, None) k t else eval env (s, i, o, None) k f
+        | While (c, b) ->
+          let config = Expr.eval env config c in
+          let s, i, o, Some r = config in
+          if r <> 0 then eval env (s, i, o, None) (Seq (stmt, k)) b else eval env (s, i, o, None) Skip k
+        | Repeat (b, c) ->
+          let config = eval env config Skip b in
+          let config = Expr.eval env config c in
+          let s, i, o, Some r = config in
+          if r = 0 then eval env (s, i, o, None) k stmt else eval env (s, i, o, None) Skip k
+        | Call (f, args) ->
+           let argvals, config = Expr.eval_list env config args in
+           let config = env#definition env f argvals config in
+           eval env config Skip k
+        | Return me -> match me with
+                      | None -> config
+                      | Some e -> Expr.eval env config e
          
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented"}
+      ifcont: c:!(Expr.parse) "then" b:stmts rest:elif { If (c, b, rest) };
+      elif:
+        -"elif" ifcont
+      | -"else" stmts -"fi"
+      | "fi" { Skip };
+      one_stmt:
+        x:IDENT ":=" e:!(Expr.parse)    { Assign (x, e) }
+      | "read" "(" x:IDENT ")"         { Read x }
+      | "write" "(" e:!(Expr.parse) ")" { Write e }
+      | -"if" ifcont
+      | "while" c:!(Expr.parse) "do" b:stmts "od" { While (c, b) }
+      | "skip" { Skip }
+      | "for" init:stmts "," c:!(Expr.parse) "," post:stmts "do" b:stmts "od" { Seq (init, While (c, Seq (b, post))) }
+      | "repeat" b:stmts "until" c:!(Expr.parse) { Repeat (b, c) }
+      | fun_name:IDENT "(" args:!(Util.list0By)[ostap (",")][Expr.parse] ")" { Call (fun_name, args) }
+      | "return" e:!(Expr.parse) { Return Some e }
+      | "return" { Return None };
+      stmts: <s::ss> : !(Util.listBy)[ostap (";")][one_stmt] { List.fold_left (fun a b -> Seq (a, b)) s ss };
+      parse: stmts
     )
       
   end
@@ -127,8 +243,9 @@ module Definition =
     (* The type for a definition: name, argument list, local variables, body *)
     type t = string * (string list * string list * Stmt.t)
 
-    ostap (     
-      parse: empty {failwith "Not implemented"}
+    ostap (
+      locals: -"local" !(Util.listBy)[ostap (",")][ostap(IDENT)] | empty {[]};
+      parse:  "fun" name:IDENT "(" params:!(Util.list0By)[ostap (",")][ostap(IDENT)] ")" l:locals "{" body:!(Stmt.parse) "}" { name, (params, l, body) }
     )
 
   end
@@ -150,7 +267,7 @@ let eval (defs, body) i =
   let _, _, o, _ =
     Stmt.eval
       (object
-         method definition env f args (st, i, o, r) =                                                                      
+         method definition env f args (st, i, o, r) =
            let xs, locs, s      =  snd @@ M.find f m in
            let st'              = List.fold_left (fun st (x, a) -> State.update x a st) (State.enter st (xs @ locs)) (List.combine xs args) in
            let st'', i', o', r' = Stmt.eval env (st', i, o, r) Stmt.Skip s in
--- src/SM.ml
+++ src/SM.ml
@@ -25,6 +25,12 @@ type prg = insn list
  *)
 type config = (prg * State.t) list * int list * Expr.config
 
+let rec split_at n l =
+  if n = 0 then [], l else
+  match l with
+  | [] -> [], []
+  | x::xs -> let h, t = split_at (n - 1) xs in x::h, t
+
 (* Stack machine interpreter
 
      val eval : env -> config -> prg -> config
@@ -32,7 +38,51 @@ type config = (prg * State.t) list * int list * Expr.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                                                  
-let rec eval env ((cstack, stack, ((st, i, o) as c)) as conf) prg = failwith "Not implemented"
+let rec eval env ((cs, st, ((s, i, o) as c)) as config) prg =
+    match prg with
+    | [] -> config
+    | (f :: rest) ->
+        match f with
+        | BINOP op ->
+            let y :: x :: st = st in
+            eval env (cs, ((Expr.to_func op) x y) :: st, c) rest
+        | CONST i ->
+            eval env (cs, i :: st, c) rest
+        | READ ->
+            let z :: i = i in
+            eval env (cs, z :: st, (s, i, o)) rest
+        | WRITE ->
+            let z :: st = st in
+            eval env (cs, st, (s, i, o @ [z])) rest
+        | LD x ->
+            eval env (cs, (State.eval s x) :: st, (s, i, o)) rest
+        | ST x ->
+            let z :: st = st in
+            eval env (cs, st, (State.update x z s, i, o)) rest
+        | LABEL l -> eval env config rest
+        | JMP l -> eval env config (env#labeled l)
+        | CJMP (t, l) ->
+            let z :: st = st in
+            let config = (cs, st, c) in
+            if ((t = "z") = (z = 0))
+            then eval env config (env#labeled l)
+            else eval env config rest
+        | BEGIN (func, params, locals) -> (* func? *)
+            let h, t = split_at (List.length params) st in
+            let s' = State.enter s (params @ locals) in
+            let s' = State.update_many params h s' in
+            eval env (cs, t, (s', i, o)) rest
+        | CALL (name, numparam, isfunc) -> (* numparam?, isfunc? *)
+           eval env ((rest, s)::cs, st, (s, i, o)) (env#labeled name)
+        | RET v ->
+           let (p', s') :: cs = cs in
+           eval env (cs, st, (State.leave s s', i, o)) p' (* v? *)
+        | END ->
+            match cs with
+            | [] -> config
+            | _  ->
+               let (p', s') :: cs = cs in
+               eval env (cs, st, (State.leave s s', i, o)) p'
 
 (* Top-level evaluation
 
@@ -50,6 +100,12 @@ let run p i =
   let m = make_map M.empty p in
   let (_, _, (_, _, o)) = eval (object method labeled l = M.find l m end) ([], [], (State.empty, i, [])) p in o
 
+let make_label_gen () =
+  let cnt = Base.ref 0 in
+  fun () ->
+    incr cnt;
+    "label" ^ (string_of_int !cnt)
+                                                                                                            
 (* Stack machine compiler
 
      val compile : Language.t -> prg
@@ -57,4 +113,53 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile (defs, p) = failwith "Not implemented"
+let compile dt =
+  let ds, t = dt in
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  | Expr.Call (name, args) -> List.flatten (List.map expr (List.rev args)) @ [CALL (name, List.length args, true)]
+  in
+  let label_gen = make_label_gen () in
+  let rec condcompile t =
+    let label = label_gen () in
+    let need, p = compile' label t in
+    p @ (if need then [LABEL label] else [])
+  and compile' outlabel t = match t with
+    | Stmt.Seq (s1, s2)  ->
+      let need, p = compile' outlabel s2 in
+      need, condcompile s1 @ p
+    | Stmt.Read x        -> false, [READ; ST x]
+    | Stmt.Write e       -> false, expr e @ [WRITE]
+    | Stmt.Assign (x, e) -> false, expr e @ [ST x]
+    | Stmt.Skip          -> false, []
+    | Stmt.If (c, t, f)  ->
+      let els = label_gen () in
+      let _, tp = compile' outlabel t in
+      let _, fp = compile' outlabel f in
+      true,
+      expr c @
+      [CJMP ("z", els)] @
+        tp @ [JMP outlabel] @
+      [LABEL els] @
+        fp
+    | Stmt.While (c, b)  ->
+      let start = label_gen () in
+      let test = label_gen () in
+      let _, bp = compile' test b in
+      false, [JMP test] @ [LABEL start] @ bp @ [LABEL test] @ expr c @ [CJMP ("nz", start)]
+    | Stmt.Repeat (b, c) ->
+      let start = label_gen () in
+      false, [LABEL start] @ condcompile b @ expr c @ [CJMP ("z", start)]
+    | Stmt.Call (name, args) -> false, List.flatten (List.map expr (List.rev args)) @ [CALL (name, List.length args, false)]
+    | Stmt.Return me -> (match me with
+                         | None -> false, [RET false]
+                         | Some e -> false, expr e @ [RET true])
+  in
+  let compile_def d =
+    let name, (params, locals, body) = d in
+    [LABEL name; BEGIN (name, params, locals)] @ condcompile body @ [END]
+  in
+  condcompile t @ [END] @
+  List.flatten (List.map compile_def ds)
